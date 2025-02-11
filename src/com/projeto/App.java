c488f7a74c256539462c44fa03bb3fba9032d682
rodrigues-i <rodrigues-i@outlook.com>
2025-02-10 21:47:21 -0300

    Imprime funcionarios na tela

--- src/com/projeto/App.java
+++ src/com/projeto/App.java
@@ -1,8 +1,17 @@
 package com.projeto;
 
+import java.util.List;
+
+import com.projeto.entidade.Funcionario;
+import com.projeto.entidade.FuncionarioHandler;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		FuncionarioHandler funcionarioHandler = new FuncionarioHandler();
+		List<Funcionario> funcionarios = funcionarioHandler.addFuncionarios();
+		for(Funcionario f: funcionarios) {
+			System.out.println(f);
+		}
 	}
 }
